ba1b5b933cac64aefb18da5a793f6df567556c77
Mert <mmertt.oozdemirr@gmail.com>
2022-05-26 15:29:00 +0300

    ilk react proje kodları

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/logo192.png" alt="" />
+      <img src={logo} alt="" />
+    </div>
+  );
+}
+
+export default App;
